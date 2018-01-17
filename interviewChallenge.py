66209247b4f084503a602e439e43f9bccef79d12
Leo <guicho.leo16@gmail.com>
2018-01-16 19:10:29 -0600

    Using recursion
    Using recursion to find all the possible outcomes.

--- interviewChallenge.py
+++ interviewChallenge.py
@@ -1,5 +1,9 @@
 from operator import itemgetter
 
+totalAmount = 25;
+
+companies = [];
+
 dataset = {'A': (1,1),
 			'B':(2,5),
 			'C':(3,8),
@@ -13,13 +17,35 @@ dataset = {'A': (1,1),
 			}
 
 #find the price per one and add to  dictionary
-for key in dataset:
-	values = dataset[key];
-	pricePerOne = float(values[0]) / values[1];
-	dataset[key]  = values + (pricePerOne,);
+# totalRequested = 0;
+# for key in dataset:
+# 	values = dataset[key];
+# 	totalRequested +=values[0];
+# 	pricePerOne = float(values[0]) / values[1];
+# 	dataset[key]  = values + (pricePerOne,);
 
-#sort dictionary by the pricePerOne
+# #sort dictionary by the pricePerOne
 
 itemsInside = dataset.items();
-itemsInside.sort(key=lambda x:x[1][2], reverse=True);
-print(itemsInside);
+# itemsInside.sort(key=lambda x:x[1][2], reverse=True);
+# print(totalRequested);
+
+# if (totalRequested == total):
+# 	print("Sell to all companies");
+# else:
+# 	recursive
+
+dataSetLength = len(itemsInside);
+
+
+def findTheCompanies(index,currentList, allLists, totalAmount):
+	if totalAmount == sum(currentList):
+		allLists.append(currentList);
+	elif totalAmount < sum(currentList):
+		return
+	for i in range(index, dataSetLength):
+		findTheCompanies(i,currentList + [itemsInside[i]],allLists,totalAmount);
+	return allLists;
+
+print(findTheCompanies(0,[],[],25));
+
